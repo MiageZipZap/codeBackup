65153e101de5d81e201715268a669b8c64ace2b5
Meriem <Meriem@172.16.0.3>
2014-12-23 14:12:59 +0100

    Add the business rules of insrastructure and equipment

--- BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/refential/infrastructure/EquipmentBusinessRules.java
+++ BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/refential/infrastructure/EquipmentBusinessRules.java
@@ -4,13 +4,32 @@ import fr.esiag.isies.pds.businessRules.IBusinessRules;
 import fr.esiag.isies.pds.model.referential.infrastructure.Equipment;
 
 /**
-* Class which list all the business rules for the Equipment referential management
-* @author ODI, GKA, MCH, PFR
-*
-*/
+ * Class which list all the business rules for the Equipment referential
+ * management
+ * 
+ * @author ODI, GKA, MCH, PFR
+ *
+ */
 public class EquipmentBusinessRules implements IBusinessRules<Equipment> {
 
 	public boolean verify(Equipment item) {
+
+		if (item == null) {
+			return false;
+		}
+
+		if (item.getLabel().length() < 2 || item.getLabel().length() > 255) {
+			return false;
+		}
+
+		if (item.getSerialNumber().length() != 10) {
+			return false;
+		}
+
+		if (!item.getTypeRefInfra().getCategory().getLabel().equals("EQUIPMENT")){
+			return false;
+		}
+	
 		return true;
 	}
 }
--- BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/refential/infrastructure/InfrastructureBusinessRules.java
+++ BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/refential/infrastructure/InfrastructureBusinessRules.java
@@ -4,14 +4,38 @@ import fr.esiag.isies.pds.businessRules.IBusinessRules;
 import fr.esiag.isies.pds.model.referential.infrastructure.Infrastructure;
 
 /**
- *Class which list all the business rules for the Infrastructure referential management
+ * Class which list all the business rules for the Infrastructure referential
+ * management
+ * 
  * @author ODI, GKA, MCH, PFR
  *
  */
-public class InfrastructureBusinessRules implements IBusinessRules<Infrastructure> {
+public class InfrastructureBusinessRules implements
+		IBusinessRules<Infrastructure> {
 
 	public boolean verify(Infrastructure item) {
+
+		if (item == null) {
+			return false;
+		}
+
+		if (item.getCapacity() < 1) {
+			return false;
+		}
+
+		if (item.getCode().length() != 10) {
+			return false;
+		}
+
+		if (item.getLabel().length() > 255 || item.getLabel().length() < 2) {
+			return false;
+		}
+
+		if (!item.getTypeRefInfra().getCategory().getLabel()
+				.equals("Infrastructure")) {
+			return false;
+		}
+
 		return true;
 	}
-	
 }
