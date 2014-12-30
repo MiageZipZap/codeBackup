fc2858ae7543ad7a2355b6e469f497baebe9e941
SamuelKoulibali <koulibali.samuel@gmail.com>
2014-12-30 07:57:38 +0100

    Business Rules 	Organization BR && Test class

--- BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/referential/organization/OrganizationBusinessRules.java
+++ BusinessRules/src/main/java/fr/esiag/isies/pds/businessRules/referential/organization/OrganizationBusinessRules.java
@@ -2,6 +2,9 @@ package fr.esiag.isies.pds.businessRules.referential.organization;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.sf.oval.ConstraintViolation;
 import net.sf.oval.Validator;
 import fr.esiag.isies.pds.businessRules.IBusinessRules;
@@ -10,15 +13,27 @@ import fr.esiag.isies.pds.model.referential.organization.Organization;
 /**
  * This class provide defined business rules verifications
  * @author Samuel
+ * TODO: remove system.out
  *
  */
 public class OrganizationBusinessRules implements IBusinessRules<Organization> {	
 	/**
 	 * 
 	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationBusinessRules.class);
+	Validator validator = new Validator();
+	List<ConstraintViolation> violations;
 	public boolean verify(Organization item) {
+		if(!isValidConstraints(item)){
+			return false;
+		}
+		//rule to verify coords value
+		if(!verifyCoords(item)){
+			return false;
+		}
+
 		//rule to verify that the siret validity
-		if(verifySiret(item.getSiret())){
+		if(!verifySiret(item.getSiret())){
 			return false;
 		};
 
@@ -28,33 +43,48 @@ public class OrganizationBusinessRules implements IBusinessRules<Organization> {
 		return true;
 	}
 
-	private boolean verifySiret(String siret){
+	public boolean verifySiret(String siret){
 		/**
 		 * TODO: will be implemented with the "search function: find by Siret method" 
 		 * and will return true if siret is found in database
 		 * OrganizationDao to be used
 		 */
+		if(siret.equals(null)){
+			LOGGER.info("Siret should not be null");
+		}
 		return true;
 
 	}
 
-	public boolean test(Organization bo){
-		//TODO: this method is a test method
-		Validator validator = new Validator();
-		// name is null
-
-		// collect the constraint violations
-		List<ConstraintViolation> violations = validator.validate(bo);
-
-		if(violations.size()>0)
-		{
-			System.out.println("Object " + bo + " is invalid.");
-			System.out.println(violations.toString());
-			return true;
-		}else{
-			System.out.println("Goooooood!");
+	public boolean verifyCoords(Organization item){
+		if(-90.0 >item.getLatitude() || item.getLatitude() >90.0){
+			LOGGER.info("latitude is not between -90.0 and 90.0");
+			System.out.println("latitude is not between -90.0 and 90.0");
+			return false;
 		}
-		return false;
+		if(-180.0 >item.getLongitude() || item.getLongitude() >180.0){
+			LOGGER.info("longitude is not between -180.0 and 180.0");
+			System.out.println("latitude is not between -180.0 and 180.0");
+			return false;
+		}
+		return true;
+	}
+
+	public boolean isValidConstraints(Organization bo){
+		violations = validator.validate(bo);
+
+		if(violations.size()>0){
+			LOGGER.error("Object " + bo + " is invalid.");
+
+			for(ConstraintViolation exception:violations){
+				LOGGER.info(exception.getMessage().toString());
+				LOGGER.info(exception.getInvalidValue().toString());
+				System.out.println(exception.getMessage().toString());
+				System.out.println(exception.getInvalidValue().toString());
+			}
+			return false;
+		}
+		return true;
 	}
 
 }
--- BusinessRules/src/test/java/fr/esiag/isies/pds/businessRules/referential/organization/OrganizationBusinessRulesTest.java
+++ BusinessRules/src/test/java/fr/esiag/isies/pds/businessRules/referential/organization/OrganizationBusinessRulesTest.java
@@ -1,16 +1,16 @@
 package fr.esiag.isies.pds.businessRules.referential.organization;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import mockit.Mocked;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import mockit.Mocked;
-import fr.esiag.isies.pds.businessRules.refential.infrastructure.InfrastructureBusinessRules;
-import fr.esiag.isies.pds.businessRules.referential.organization.OrganizationBusinessRules;
-import fr.esiag.isies.pds.dao.referential.infrastructure.CategoryRefInfraDao;
 import fr.esiag.isies.pds.dao.referential.organization.OrganizationDao;
-import fr.esiag.isies.pds.model.referential.infrastructure.CategoryRefInfra;
-import fr.esiag.isies.pds.model.referential.infrastructure.Infrastructure;
-import fr.esiag.isies.pds.model.referential.infrastructure.TypeRefInfra;
+import fr.esiag.isies.pds.model.referential.organization.OrgaType;
 import fr.esiag.isies.pds.model.referential.organization.Organization;
 
 /**
@@ -20,55 +20,88 @@ import fr.esiag.isies.pds.model.referential.organization.Organization;
  */
 public class OrganizationBusinessRulesTest{
 	/**
+	 *LOGGER
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrganizationBusinessRules.class);
+	/**
 	 * Organization Business rule Tested
 	 */
 	private OrganizationBusinessRules orgaBR = new OrganizationBusinessRules(); 
 
 	/**
-	 * Valid organization
+	 * Valid
 	 */
 	private Organization validOrganization;
+	private OrgaType validOrganizationType;
 
 	/**
-	 * Wrong organiazation
+	 * Wrong
 	 */
-	private Organization wrongOrganization;
+	private Organization wrongOrganizationConstraints;
 
 	/**
 	 *---------Elements to test--------- 
 	 */
-	//organization type
-	private Organization wrongOrganizationType;
-	//name
-	private Organization wrongOrganizationName;
+	//Forms constraints wrongOrganizationConstraint
+	
 	//siret
 	private Organization wrongOrganizationSiret;
 	
-	//TODO:adress class will be created and corresponding Test Class
-	
-	//gps coords
-	private Organization wrongOrganizationLatitude;
-	private Organization wrongOrganizationLongitude;
-	
-	//gps contact
-	private Organization wrongOrganizationFax;
-	private Organization wrongOrganizationEmail;
-	private Organization wrongOrganizationPhone;
-	
-	/**
-	 * 
+	//TODO:organization id
+	/** 
 	 * Mocked DAO
 	 */
 	@Mocked
 	OrganizationDao orgaDao;
-	
+
 	@Before
 	public void prepareTest() {
-		
+		//OrganizationType
+		validOrganizationType = new OrgaType();
+		validOrganizationType.setCode("OT02");
+		validOrganizationType.setLabel("Hôpital");
+		validOrganizationType.setLegalStatus("public");
+		//Organization
+		validOrganization = new Organization();
+		validOrganization.setOrgaType(validOrganizationType);
+		validOrganization.setName("Mondor");
+		validOrganization.setSiret("12345678912345");
+		validOrganization.setStreetNumber(83);
+		validOrganization.setStreetType("rue");
+		validOrganization.setStreetName("petit");
+		validOrganization.setCity("Paris");
+		validOrganization.setZipCode("75019");
+		validOrganization.setDepartment("ile de france");
+		validOrganization.setLatitude((float) 48.56325);
+		validOrganization.setLongitude((float) 158.56325);
+		validOrganization.setFax("0142515252");
+		validOrganization.setEmail("samuel@koulibali.com");
+		validOrganization.setPhone("0142515252");
+
+
+
+		// Wrong Organization
+		wrongOrganizationConstraints = new Organization();
+		wrongOrganizationConstraints.setOrgaType(validOrganizationType);
+		wrongOrganizationConstraints.setName("M");
+		wrongOrganizationConstraints.setSiret("1234567891234");
+		wrongOrganizationConstraints.setStreetNumber(83);
+		wrongOrganizationConstraints.setStreetType(".");
+		wrongOrganizationConstraints.setStreetName("t");
+		wrongOrganizationConstraints.setCity("j");
+		wrongOrganizationConstraints.setZipCode("k75019");
+		wrongOrganizationConstraints.setDepartment("i");
+		wrongOrganizationConstraints.setLatitude((float) -900);
+		wrongOrganizationConstraints.setLongitude((float) 1585.563251111);
+		wrongOrganizationConstraints.setFax("014251522");
+		wrongOrganizationConstraints.setEmail("samuel@koulibali");
+		wrongOrganizationConstraints.setPhone("0142515252$");
 	}
-	
+
 	@Test
 	public void executeTest() {
-		
+		assertTrue(orgaBR.verify(this.validOrganization));
+		assertFalse(orgaBR.verify(this.wrongOrganizationConstraints));
+
 	}
 }
