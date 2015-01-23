fc421ddd6d65260f09909c31fc9145021ec3ff6c
ADHEPEAU <adhepeau.lionel@gmail.com>
2015-01-23 14:21:09 +0100

    Commit du StaffControler

--- easyessoft/src/main/fr/esiag/isies/pds/controller/referential/Staff/StaffController.java
+++ easyessoft/src/main/fr/esiag/isies/pds/controller/referential/Staff/StaffController.java
@@ -1,14 +1,10 @@
 package fr.esiag.isies.pds.controller.referential.Staff;
 
-import fr.esiag.isies.pds.model.referential.staff.StaffMember;
 import fr.esiag.isies.pds.dao.referential.staff.StaffMemberDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Get the HTTP request, do actions (Database) and return a view (JSP)
@@ -27,23 +23,9 @@ public class StaffController {
 	 * @return form to create a Staff
 	 */
 	@RequestMapping("/staffHome")
-	public String getForm(Model model){
-		model.addAttribute(new StaffMember());
-		LOGGER.info("EASYES Form display :  creation ");
-		return "ref/staff/staffMemberCreate";
+	public String getMenu(){
+		LOGGER.info("EASYES Administration menu page of Staff Member");
+		return "ref/staff/menuStaffPage";
 	}
 	
-	/**
-	 * @param Staff
-	 * @param model
-	 * @return a confirmation of Staff creation
-	 */
-	@RequestMapping(value="/create",method=RequestMethod.POST)
-	public String create(@ModelAttribute StaffMember StaffMember,Model model){
-	//	Staff.setUpdateUser(SecurityContextHolder.getContext().getAuthentication().getName());
-			staffMemberDAO.create(StaffMember);
-			model.addAttribute("Staff",StaffMember);
-			LOGGER.info("EASYES Staff creation OK");
-			return "ref/staff/menuStaffPage.jsp";		
-	}
 }
