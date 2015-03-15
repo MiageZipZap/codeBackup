b2482604ee617219acddbc93e1cc57f82e07136a
oriel <oriel@192.168.0.12>
2015-03-15 19:49:55 +0100

    EasyEsSoft: 	hibernate.cfg.xml: NOT PERSONAL CONFIG: Add emergency_queue_optim.xml 	Add :emergency_queue_optim.xml Model: 	EmergencyFrequentationTrafficLog: Change class because of scope's 	changes

--- Model/src/main/java/fr/esiag/isies/pds/model/optimisation/EmergencyFrequentationTrafficLog.java
+++ Model/src/main/java/fr/esiag/isies/pds/model/optimisation/EmergencyFrequentationTrafficLog.java
@@ -1,152 +1,77 @@
 package fr.esiag.isies.pds.model.optimisation;
 
+import java.util.Date;
+
 import fr.esiag.isies.pds.model.AbstractEntity;
+import fr.esiag.isies.pds.model.referential.infrastructure.Infrastructure;
 import fr.esiag.isies.pds.model.referential.organization.Hospital;
 
-/**
- * Statement of admission for patient and statement average of time for exam to patient
- * @author oriel
- *
- */
-public class EmergencyFrequentationTrafficLog extends AbstractEntity {
-	/**
-	 * Time Slot of statement
-	 */
-	private String representativeTimeSlot;
-	/**
-	 * In this Time-slot, number of patients of priority one which are admitted in emergency service 
-	 */
-	private int nbAdmissionForPriorityOne;
-	/**
-	 * In this Time-slot, number of patients of priority two which are admitted in emergency service
-	 */
-	private int nbAdmissionForPriorityTwo;
-	/**
-	 * In this Time-slot, number of patients of priority three which are admitted in emergency service
-	 */
-	private int nbAdmissionForPriorityThree;
-	/**
-	 * In this Time-slot, number of patients of priority four which are admitted in emergency service
-	 */
-	private int nbAdmissionForPriorityFour;
-	/**
-	 * In this Time-slot, number of patients of priority five which are admitted in emergency service
-	 */
-	private int nbAdmissionForPriorityFive;
-	/**
-	 * Hopital wich whose the statement are provide
-	 */
-	private Hospital evaluateHospital; 
-	/**
-	 * For priority one's patient which have a consultation with doctor, average time of this consultation
-	 */
-	private int examTimeForPriorityOnePatient;
-	/**
-	 * For priority two's patient which have a consultation with doctor, average time of this consultation
-	 */
-	private int examTimeForPriorityTwoPatient;
-	/**
-	 * For priority three's patient which have a consultation with doctor, average time of this consultation
-	 */
-	private int examTimeForPriorityThreePatient;
-	/**
-	 * For priority four's patient which have a consultation with doctor, average time of this consultation
-	 */
-	private int examTimeForPriorityFourPatient;
-	/**
-	 * For priority five's patient which have a consultation with doctor, average time of this consultation
-	 */
-	private int examTimeForPriorityFivePatient;
-	/**
-	 * Constructor of EmergencyFrequentation
-	 */
-	public EmergencyFrequentationTrafficLog() {
-		super();
+public class EmergencyFrequentationTrafficLog extends AbstractEntity{
+	
+	Hospital hospital;
+	int idPatient;
+	int idDoctor;
+	Infrastructure infra;
+	Date dateInputPatient;
+	Date dateExitBox;
+	
+	public EmergencyFrequentationTrafficLog(Hospital hospital, int idPatient, int idDoctor, Infrastructure infra,
+		 Date dateInputPatient,Date dateExitBox) {
+		this.hospital = hospital;
+		this.idPatient = idPatient;
+		this.idDoctor = idDoctor;
+		this.infra = infra;
+		this.dateInputPatient = dateInputPatient;
+		this.dateExitBox = dateExitBox;
 	}
-	/**
-	 * 
-	 * @return
-	 */
-	public String getRepresentativeTimeSlot() {
-		return representativeTimeSlot;
+
+	public Hospital getHospital() {
+		return hospital;
 	}
-	/**
-	 * Set a representative time slot for a statement
-	 * @param representativeTimeSlot
-	 */
-	public void setRepresentativeTimeSlot(String representativeTimeSlot) {
-		this.representativeTimeSlot = representativeTimeSlot;
+
+	public void setHospital(Hospital hospital) {
+		this.hospital = hospital;
 	}
-	/**
-	 * 
-	 * @return number of admission for priority one
-	 */
-	public int getNbAdmissionForPriorityOne() {
-		return nbAdmissionForPriorityOne;
+
+	public int getIdPatient() {
+		return idPatient;
 	}
-	public void setNbAdmissionForPriorityOne(int nbAdmissionForPriorityOne) {
-		this.nbAdmissionForPriorityOne = nbAdmissionForPriorityOne;
+
+	public void setIdPatient(int idPatient) {
+		this.idPatient = idPatient;
 	}
-	public int getNbAdmissionForPriorityTwo() {
-		return nbAdmissionForPriorityTwo;
+
+	public int getIdDoctor() {
+		return idDoctor;
 	}
-	public void setNbAdmissionForPriorityTwo(int nbAdmissionForPriorityTwo) {
-		this.nbAdmissionForPriorityTwo = nbAdmissionForPriorityTwo;
+
+	public void setIdDoctor(int idDoctor) {
+		this.idDoctor = idDoctor;
 	}
-	public int getNbAdmissionForPriorityThree() {
-		return nbAdmissionForPriorityThree;
+
+	public Infrastructure getInfra() {
+		return infra;
 	}
-	public void setNbAdmissionForPriorityThree(int nbAdmissionForPriorityThree) {
-		this.nbAdmissionForPriorityThree = nbAdmissionForPriorityThree;
+
+	public void setInfra(Infrastructure infra) {
+		this.infra = infra;
 	}
-	public int getNbAdmissionForPriorityFour() {
-		return nbAdmissionForPriorityFour;
+
+	public Date getDateInputPatient() {
+		return dateInputPatient;
 	}
-	public void setNbAdmissionForPriorityFour(int nbAdmissionForPriorityFour) {
-		this.nbAdmissionForPriorityFour = nbAdmissionForPriorityFour;
+
+	public void setDateInputPatient(Date dateInputPatient) {
+		this.dateInputPatient = dateInputPatient;
 	}
-	public int getNbAdmissionForPriorityFive() {
-		return nbAdmissionForPriorityFive;
+
+	public Date getDateExitBox() {
+		return dateExitBox;
 	}
-	public void setNbAdmissionForPriorityFive(int nbAdmissionForPriorityFive) {
-		this.nbAdmissionForPriorityFive = nbAdmissionForPriorityFive;
+
+	public void setDateExitBox(Date dateExitBox) {
+		this.dateExitBox = dateExitBox;
 	}
-	public Hospital getEvaluateHospital() {
-		return evaluateHospital;
-	}
-	public void setEvaluateHospital(Hospital evaluateHospital) {
-		this.evaluateHospital = evaluateHospital;
-	}
-	public int getExamTimeForPriorityOnePatient() {
-		return examTimeForPriorityOnePatient;
-	}
-	public void setExamTimeForPriorityOnePatient(int examTimeForPriorityOnePatient) {
-		this.examTimeForPriorityOnePatient = examTimeForPriorityOnePatient;
-	}
-	public int getExamTimeForPriorityTwoPatient() {
-		return examTimeForPriorityTwoPatient;
-	}
-	public void setExamTimeForPriorityTwoPatient(int examTimeForPriorityTwoPatient) {
-		this.examTimeForPriorityTwoPatient = examTimeForPriorityTwoPatient;
-	}
-	public int getExamTimeForPriorityThreePatient() {
-		return examTimeForPriorityThreePatient;
-	}
-	public void setExamTimeForPriorityThreePatient(
-			int examTimeForPriorityThreePatient) {
-		this.examTimeForPriorityThreePatient = examTimeForPriorityThreePatient;
-	}
-	public int getExamTimeForPriorityFourPatient() {
-		return examTimeForPriorityFourPatient;
-	}
-	public void setExamTimeForPriorityFourPatient(int examTimeForPriorityFourPatient) {
-		this.examTimeForPriorityFourPatient = examTimeForPriorityFourPatient;
-	}
-	public int getExamTimeForPriorityFivePatient() {
-		return examTimeForPriorityFivePatient;
-	}
-	public void setExamTimeForPriorityFivePatient(int examTimeForPriorityFivePatient) {
-		this.examTimeForPriorityFivePatient = examTimeForPriorityFivePatient;
-	}
+
 	
 }
