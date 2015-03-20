cca99e770e8b4ab84ea5921c35b496dc68210001
oriel <oriel@172.16.131.1>
2015-03-20 18:01:47 +0100

    EasyEs: 	Correction of EmergencytrafficLogDao

--- Dao/src/main/java/fr/esiag/isies/pds/dao/optimisation/Emergency/EmergencyFrequentationTrafficLogDao.java
+++ Dao/src/main/java/fr/esiag/isies/pds/dao/optimisation/Emergency/EmergencyFrequentationTrafficLogDao.java
@@ -1,10 +1,16 @@
 package fr.esiag.isies.pds.dao.optimisation.Emergency;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import fr.esiag.isies.pds.dao.AbstractEntityDao;
+import fr.esiag.isies.pds.model.AbstractEntity;
+import fr.esiag.isies.pds.model.optimisation.EmergencyFrequentationTrafficLog;
+import fr.esiag.isies.pds.model.referential.organization.Organization;
+import fr.esiag.isies.pds.model.referential.organization.ServiceType;
 import fr.esiag.isies.pds.utils.HibernateUtil;
 
 public class EmergencyFrequentationTrafficLogDao{
@@ -18,4 +24,14 @@ public class EmergencyFrequentationTrafficLogDao{
 		return lst;
 	}
 	
+
+	public ArrayList<EmergencyFrequentationTrafficLog> getById(int id) {
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		session.beginTransaction();
+		Query query = session.createQuery("from EmergencyFrequentationTrafficLog where hospital.id = :id ");
+		query.setParameter("id", id);
+		ArrayList<EmergencyFrequentationTrafficLog> lst = (ArrayList<EmergencyFrequentationTrafficLog>) query.list();
+		session.close();
+		return lst;
+	}
 }
