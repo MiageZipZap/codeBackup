b1bb57c08b37aa9782a53e5de671f32332174224
oriel <oriel@192.168.84.1>
2015-03-26 11:03:48 +0100

    Model:
    ==>AnalyzeResultEmergencyService and EmergencyFrequentationTrafficLog :
    add comments

--- Model/src/main/java/fr/esiag/isies/pds/model/optimisation/AnalyzeResultEmergencyService.java
+++ Model/src/main/java/fr/esiag/isies/pds/model/optimisation/AnalyzeResultEmergencyService.java
@@ -1,25 +1,49 @@
 package fr.esiag.isies.pds.model.optimisation;
 
-
+/**
+ * Class wich we have an hour and patient who are in emergency service
+ * @author OSA
+ *
+ */
 public class AnalyzeResultEmergencyService {
 	
 	private String hourOfPassage;
 	private Long nbwaitingPatient;
-	
+	/**
+	 * Construct of class , init with an analyze hour and number of patient wich attending in emergency service 
+	 * @param hourOfPassage
+	 * @param nbwaitingPatient
+	 */
 	public AnalyzeResultEmergencyService(String hourOfPassage, Long nbwaitingPatient) {
 		super();
 		this.hourOfPassage = hourOfPassage;
 		this.nbwaitingPatient = nbwaitingPatient;
 	}
+	/**
+	 * Method wich return hour of analyze
+	 * @return String hour
+	 */
 	public String gethourOfPassage() {
 		return hourOfPassage;
 	}
+	/**
+	 * Method to make an hour of analyze
+	 * @param hourOfPassage
+	 */
 	public void sethourOfPassage(String hourOfPassage) {
 		this.hourOfPassage = hourOfPassage;
 	}
+	/**
+	 * Method wich return nb patient for an hour
+	 * @return Long 
+	 */
 	public Long getNbWaitingPatient() {
 		return nbwaitingPatient;
 	}
+	/**
+	 * Method to when we recovery a number of patient for analyze
+	 * @param nbWaitingPatient
+	 */
 	public void setNbWaitingPatient(Long nbWaitingPatient) {
 		this.nbwaitingPatient = nbWaitingPatient;
 	}
--- Model/src/main/java/fr/esiag/isies/pds/model/optimisation/EmergencyFrequentationTrafficLog.java
+++ Model/src/main/java/fr/esiag/isies/pds/model/optimisation/EmergencyFrequentationTrafficLog.java
@@ -5,7 +5,11 @@ import java.util.Date;
 import fr.esiag.isies.pds.model.AbstractEntity;
 import fr.esiag.isies.pds.model.referential.infrastructure.Infrastructure;
 import fr.esiag.isies.pds.model.referential.organization.Hospital;
-
+/**
+ * Class wich represent an activity of patient in emergency service
+ * @author OSA
+ *
+ */
 public class EmergencyFrequentationTrafficLog extends AbstractEntity{
 	
 	Hospital hospital;
@@ -16,7 +20,15 @@ public class EmergencyFrequentationTrafficLog extends AbstractEntity{
 	Date dateExitBox;
 	
 	public EmergencyFrequentationTrafficLog(){}
-	
+	/**
+	 * Constructor of object to create an activity
+	 * @param hospital
+	 * @param idPatient
+	 * @param idDoctor
+	 * @param infra
+	 * @param dateInputPatient
+	 * @param dateExitBox
+	 */
 	public EmergencyFrequentationTrafficLog(Hospital hospital, int idPatient, int idDoctor, Infrastructure infra,
 		 Date dateInputPatient,Date dateExitBox) {
 		this.hospital = hospital;
@@ -26,51 +38,87 @@ public class EmergencyFrequentationTrafficLog extends AbstractEntity{
 		this.dateInputPatient = dateInputPatient;
 		this.dateExitBox = dateExitBox;
 	}
-
+	/**
+	 * Method wich return Hospital for an activity of patient in emergency service
+	 * @return Hospital
+	 */
 	public Hospital getHospital() {
 		return hospital;
 	}
-
+	/**
+	 * Method wich we can set an Hospital for an activity of patient in emergency service
+	 * @param hospital
+	 */
 	public void setHospital(Hospital hospital) {
 		this.hospital = hospital;
 	}
-
+	/**
+	 * Method wich return id of patient for an activity of patient in emergency service
+	 * @return idPatient
+	 */
 	public int getIdPatient() {
 		return idPatient;
 	}
-
+	/**
+	 * Method wich we can set an id of patient for an activity of patient in emergency service
+	 * @param idPatient
+	 */
 	public void setIdPatient(int idPatient) {
 		this.idPatient = idPatient;
 	}
-
+	/**
+	 * Method wich return id of doctor for an activity of patient in emergency service
+	 * @return idDoctor
+	 */
 	public int getIdDoctor() {
 		return idDoctor;
 	}
-
+	/**
+	 * Method wich we can set an id of doctor for an activity of patient in emergency service
+	 * @param idDoctor
+	 */
 	public void setIdDoctor(int idDoctor) {
 		this.idDoctor = idDoctor;
 	}
-
+	/**
+	 * Method wich return an infrastructure (exam box) for an activity of patient in emergency service
+	 * @return infra
+	 */
 	public Infrastructure getInfra() {
 		return infra;
 	}
-
+	/**
+	 * Method wich we can set Infrastructure (exam box) for an activity of patient in emergency service
+	 * @param infra
+	 */
 	public void setInfra(Infrastructure infra) {
 		this.infra = infra;
 	}
-
+	/**
+	 * Method wich return a date of input patient for an activity of patient in emergency service
+	 * @return dateInputPatient
+	 */
 	public Date getDateInputPatient() {
 		return dateInputPatient;
 	}
-
+	/**
+	 * Method wich we can set a date of input patient for an activity of patient in emergency service
+	 * @param dateInputPatient
+	 */
 	public void setDateInputPatient(Date dateInputPatient) {
 		this.dateInputPatient = dateInputPatient;
 	}
-
+	/**
+	 * Method wich return a date of exit of a exam box for an activity of patient in emergency service
+	 * @return dateExitBox
+	 */
 	public Date getDateExitBox() {
 		return dateExitBox;
 	}
-
+	/**
+	 * Method wich we can set a date  exit of a exam box for an activity of patient in emergency service
+	 * @param dateExitBox
+	 */
 	public void setDateExitBox(Date dateExitBox) {
 		this.dateExitBox = dateExitBox;
 	}
